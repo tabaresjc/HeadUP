142601e492b4b4c5d11bbf0f78a502ef775e3455
Juan Tabares <juan.ctt@live.com>
2014-01-11 23:36:14 +0900

    added categorie controller

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,17 @@
-from flask import Flask, render_template, flash, redirect, session, url_for, request, g, jsonify
+from flask import Flask
 from flask.ext.login import LoginManager
-from flask.ext.classy import FlaskView
-from flask.ext.principal import Principal
 from flask_wtf.csrf import CsrfProtect
 from flask.ext.babel import Babel, lazy_gettext
-from storm.locals import create_database, Store, ReferenceSet, Reference, Desc
+from storm.locals import create_database, Store, ReferenceSet, Desc
 from config import STORM_DATABASE_URI
 from utilities import Utilities
 
 import os
 
-
 app = Flask(__name__)
 if os.environ.get('HEROKU') is None:
-	app.debug = True
+  app.debug = True
+
 # Load the app's configuration
 app.config.from_object('config')
 app.jinja_env.trim_blocks = True
@@ -39,13 +37,13 @@ login_manager.login_message = lazy_gettext('Please log in to access this page.')
 # controllers
 #----------------------------------------
 import blog.views
-import admin.views
+
 
 # register the sessions module blueprint
 from app.sessions.views import mod as sessionsModule
 app.register_blueprint(sessionsModule, url_prefix='/members')
 
-# register the User module 
+# register the User module
 from app.users.views import UsersView
 UsersView.register(app)
 
@@ -65,10 +63,10 @@ from app.comments.models import Comment
 
 
 # User.posts = ReferenceSet(User.id, UserPosts.user_id, UserPosts.post_id, Post.id)
-User.posts = ReferenceSet(User.id, Post.user_id, order_by = Desc(Post.id))	
-Post.comments = ReferenceSet(Post.id, Comment.post_id, order_by = Comment.id)
-User.comments = ReferenceSet(User.id, Comment.user_id, order_by = Desc(Comment.id))
-Comment.replies = ReferenceSet(Comment.id, Comment.comment_id, order_by = Comment.id)
+User.posts = ReferenceSet(User.id, Post.user_id, order_by=Desc(Post.id))
+Post.comments = ReferenceSet(Post.id, Comment.post_id, order_by=Comment.id)
+User.comments = ReferenceSet(User.id, Comment.user_id, order_by=Desc(Comment.id))
+Comment.replies = ReferenceSet(Comment.id, Comment.comment_id, order_by=Comment.id)
 
 #----------------------------------------
 # filters
@@ -80,8 +78,7 @@ app.jinja_env.filters['htmltruncate'] = Utilities.htmltruncate
 app.jinja_env.filters['get_stat'] = blog.models.get_stat
 app.jinja_env.filters['sidebar'] = Utilities.get_navigation_bar
 
-
 if app.debug:
-	import sys
-	from storm.tracer import debug
-	debug(True, stream=sys.stdout)
+    import sys
+    from storm.tracer import debug
+    debug(True, stream=sys.stdout)
