758479399a13d4bdf2fec681d34b4e145d6f1ae4
Juan Tabares <juan.ctt@live.com>
2016-05-20 14:52:55 +0900

    fix category's model

--- app/categories/models.py
+++ app/categories/models.py
@@ -3,7 +3,6 @@
 from flask.ext.login import current_user
 from app import db
 from app.utils.db import ModelBase, MutableDict
-from app.posts.models import Post
 import datetime
 
 
@@ -40,27 +39,33 @@ class Category(db.Model, ModelBase):
 
     @classmethod
     def transfer_posts(cls, from_category, to_category=None):
+      from app.posts.models import Post
       result, count = cls.pagination(limit=1, desc=False)
       if count <= 1:
         return False
       if not to_category:
         to_category = result.one()
-      store.find(Post, Post.category_id == from_category.id).set(category_id=to_category.id)
-      store.commit()
+
+      Post.query.filter_by(category_id=from_category.id).update(dict(category_id=to_category.id))
+
+      db.session.commit()
       return True
 
     @classmethod
     def get_by_cat_slug(cls, cat, slug):
-      return store.find(Category,
-        Category.slug == unicode(cat)).one().posts.find(Post.slug == unicode(slug)).one()
+      category = cls.query.filter_by(slug=cat).one_or_none()
+      if not category:
+          return None
+      return category.posts
 
     @classmethod
     def get_by_cat(cls, cat):
-      return store.find(Category, Category.slug == unicode(cat)).one()
+      return cls.query.filter_by(slug=cat).one_or_none()
 
     @classmethod
     def get_posts_by_cat(cls, cat, limit=10, page=1, desc=True):
-      category = store.find(Category, Category.slug == unicode(cat)).one()
-      offset = (page - 1) * limit
-      bounderie = offset + limit
-      return category.posts.find()[offset:bounderie], category
+      category = cls.query.filter_by(slug=cat).one_or_none()
+      if not category:
+          return None, None
+      posts = category.posts.order_by(db.text('id DESC')).limit(limit).offset((page - 1) * limit)
+      return posts, category
