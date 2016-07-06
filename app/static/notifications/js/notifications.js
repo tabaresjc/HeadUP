9b604de54f898e53e4cd74ca21b58696b750f6c0
Juan Tabares <juan.ctt@live.com>
2016-07-06 19:20:43 +0900

    improve the notification system a bit

--- app/static/notifications/js/notifications.js
+++ app/static/notifications/js/notifications.js
@@ -8,6 +8,9 @@
     parentSelector           : 'body',
     // message selectors
     messageTextSelectors     : '#message-list > div',
+    // translate the category used on the system to the one use on this library
+    messageTypes             : {message: 'success', error: 'error', warning: 'warning'},
+    notifierPosition         : 'bottom-right',
   };
   
   // Class Constructor
@@ -21,28 +24,36 @@
       this.element = element;
       // Load the messages
       this.messageTextSelectors  = $(this.options.parentSelector + ' ' + this.options.messageTextSelectors);
+      
       this.setupSystemNotifications();
     },
     setupSystemNotifications: function() {
+      // Load the types
+      var messageTypes = this.options.messageTypes;
+      // set the position of the notifier
+      alertify.set('notifier','position', this.options.notifierPosition);
+
       this.messageTextSelectors.each(function(index, element) {
-        var category = $(element).data('category'),
+        var name = $(element).data('category'),
             text = $(element).text();
-        var notification = alertify.notify(text, 'success', 5);
+        var messageType = messageTypes[name];
+        var notification = alertify.notify(text, messageType, 5);
       });
     }
   };
   
   // A really lightweight plugin wrapper around the constructor,
   // preventing against multiple instantiations
-  // $.fn[pluginName] = function (options) {
-  //   return this.each(function () {
-  //     if (!$.data(this, "plugin_" + pluginName)) {
-  //       $.data(this, "plugin_" + pluginName, new Notification(this, options));
-  //     }
-  //   });
-  // };
+  $.fn[pluginName] = function (options) {
+    return this.each(function () {
+      if (!$.data(this, "plugin_" + pluginName)) {
+        $.data(this, "plugin_" + pluginName, new Notification(this, options));
+      }
+    });
+  };
 
   $(function() {
-    var notif = new Notification();
+    // load & display the system messages once is ready
+    $('#message-list').notifications();
   });
 })(jQuery, window, document);
