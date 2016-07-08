d2e49af0e601a1fa4c6eaa7e33e980f261a46a5a
Juan Tabares <juan.ctt@live.com>
2016-07-08 12:13:23 +0900

    relocate templates to original position

--- app/__init__.py
+++ app/__init__.py
@@ -19,10 +19,10 @@ import os
 app = Flask(__name__)
 
 # Set the location of the templates
-app.jinja_loader = jinja2.ChoiceLoader([
-    app.jinja_loader,
-    jinja2.FileSystemLoader('templates'),
-])
+# app.jinja_loader = jinja2.ChoiceLoader([
+#     app.jinja_loader,
+#     jinja2.FileSystemLoader('templates'),
+# ])
 
 # -------------------------------------------------------------------------
 # Load the app's configuration
