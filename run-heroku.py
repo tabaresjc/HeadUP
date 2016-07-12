92c944c83fc1ae1cf1315feda98849391754a04c
Juan Tabares <juan.ctt@live.com>
2016-07-12 21:39:32 +0900

    nginx server configuration

--- run-heroku.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf8 -*-
-
-from app import app
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 # -*- coding: utf8 -*-
 
-from app import app as application
+from app import app
 
 if __name__ == "__main__":
     application.run()
