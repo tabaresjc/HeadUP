3dbc80473c5500f93eb32651fa73ee72395ad934
Juan Tabares <juan.ctt@live.com>
2016-10-30 18:34:44 +0900

    Update user forms

--- app/admin/users/index.py
+++ app/admin/users/index.py
@@ -12,131 +12,109 @@ from forms import UserForm, EditUserForm, NewUserForm
 
 
 class UsersView(FlaskView):
-  route_base = '/mypage/users'
-  decorators = [login_required]
+    route_base = '/mypage/users'
+    decorators = [login_required]
 
-  def index(self):
-    page = request.values.get('page', 1, type=int)
-    limit = 20
-    users, total = User.pagination(page=page, limit=limit,
-                                   orderby='users.id', desc=False)
+    def index(self):
+        page = request.values.get('page', 1, type=int)
+        limit = 20
+        users, total = User.pagination(page=page, limit=limit,
+                                       orderby='users.id', desc=False)
+        if not users.count() and page > 1:
+            return redirect(url_for('UsersView:index_1'))
+        return render_template('admin/users/index.html',
+                               users=users,
+                               page=page,
+                               limit=limit,
+                               total=total)
 
-    if not users.count() and page > 1:
-      return redirect(url_for('UsersView:index_1'))
+    def get(self, id):
+        user = User.get_by_id(id)
 
-    return render_template('admin/users/index.html',
-                           users=users,
-                           page=page,
-                           limit=limit,
-                           total=total)
+        if user is None or not user.can_edit():
+            flash(gettext('The user was not found'), 'error')
+            return redirect(url_for('UsersView:index'))
 
-  def get(self, id):
-    user = User.get_by_id(id)
+        return render_template('admin/users/show.html', user=user)
 
-    if user is None or not user.can_edit():
-      flash(gettext('The user was not found'), 'error')
-      return redirect(url_for('UsersView:index'))
+    @route('/new', methods=['GET', 'POST'])
+    def post(self):
+        form = NewUserForm()
 
-    return render_template('admin/users/show.html', user=user)
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                user = User.create()
+                form.populate_obj(user)
+                user.set_password(form.password.data)
+                user.save()
 
-  @route('/', methods=['POST'])
-  @route('/new', methods=['GET'])
-  def post(self):
-    if request.method == 'POST':
-      form = NewUserForm()
-      if form.validate_on_submit():
+                flash(gettext('User was succesfully saved'))
+                return redirect(url_for('UsersView:get', id=user.id))
+            else:
+                flash(gettext('Invalid submission, please check the messages below'), 'error')
+
+        return render_template('admin/users/add.html', form=form, user=None)
+
+    @route('/edit/<int:id>', methods=['GET', 'POST'])
+    def put(self, id):
+        user = User.get_by_id(id)
+
+        if user is None or not user.can_edit():
+            flash(gettext('The user was not found'), 'error')
+            return redirect(url_for('UsersView:index'))
+
+        form = EditUserForm(user)
+        if request.method in ['POST']:
+            if form.validate_on_submit():
+                if form.password.data:
+                    user.set_password(form.password.data)
+                del form.password
+                form.populate_obj(user)
+                user.save()
+                refresh()
+                flash(gettext('User was succesfully saved'))
+                return redirect(url_for('UsersView:get', id=user.id))
+            else:
+                flash(gettext('Invalid submission, please check the messages below'), 'error')
+
+        return render_template('admin/users/edit.html', form=form, user=user)
+
+    @route('/remove/<int:id>', methods=['POST', 'DELETE'])
+    def delete(self, id):
+
+        user = User.get_by_id(id)
+
+        if user is None or not user.can_edit():
+            abort(401)
+
+        if current_user.id == user.id:
+            abort(403)
+
         try:
-          user = User.create()
-          form.populate_obj(user)
-          user.set_password(form.password.data)
-          user.save()
-
-          flash(gettext('User was succesfully saved'))
-          return redirect(url_for('UsersView:get', id=user.id))
+            name = user.name
+            User.delete(user.id)
+            flash(gettext('The user "%(name)s" was removed', name=name))
         except Exception as e:
-          flash(gettext('Error while creating the user, %(error)', error=e), 'error')
-      else:
-        flash(
-            gettext('Invalid submission, please check the messages below'), 'error')
-    else:
-      form = NewUserForm()
+            flash(gettext('Error while removing the user, %(error)s', error=e), 'error')
 
-    return render_template('admin/users/add.html', form=form, user=None)
+        return redirect(url_for('UsersView:index'))
 
-  @route('/<int:id>', methods=['PUT'])
-  @route('/edit/<int:id>', methods=['GET', 'POST'])
-  def put(self, id):
-    user = User.get_by_id(id)
+    @route('/<int:id>/posts/', endpoint='user_post')
+    def user_post(self, id):
+        user = User.get_by_id(id)
 
-    if user is None or not user.can_edit():
-      flash(gettext('The user was not found'), 'error')
-      return redirect(url_for('UsersView:index'))
+        if user is None:
+            flash(gettext('The user was not found'), 'error')
+            return redirect(url_for('UsersView:index'))
 
-    if request.method in ['POST', 'PUT']:
-      form = UserForm()
-      if form.validate_on_submit():
-        try:
-          if form.password.data:
-            user.set_password(form.password.data)
-          del form.password
-          form.populate_obj(user)
-          user.save()
-          refresh()
-          flash(gettext('User was succesfully saved'))
-          if request.method == 'POST':
-            return redirect(url_for('UsersView:get', id=user.id))
-        except Exception as e:
-          flash(gettext('Error while updating the user, %(error)', error=error), 'error')
-      else:
-        flash(gettext('Invalid submission, please check the messages below'), 'error')
+        page = request.values.get('page', 1, type=int)
+        limit = 10
 
-      if request.method == 'PUT':
-        return jsonify(redirect_to=url_for('UsersView:index'))
-    else:
-      form = EditUserForm(user)
+        posts, total = user.get_user_posts(page=page, limit=limit)
 
-    return render_template('admin/users/edit.html', form=form, user=user)
-
-  @route('/<int:id>', methods=['DELETE'])
-  @route('/remove/<int:id>', methods=['POST'])
-  def delete(self, id):
-
-    user = User.get_by_id(id)
-
-    if user is None or not user.can_edit():
-      abort(401)
-
-    if current_user.id == user.id:
-      abort(403)
-
-    try:
-      name = user.name
-      User.delete(user.id)
-      flash(gettext('The user "%(name)s" was removed', name=name))
-    except Exception as e:
-      flash(gettext('Error while removing the user, %(error)s', error=e), 'error')
-
-    if request.method == 'DELETE':
-      return jsonify(redirect_to=url_for('UsersView:index'))
-
-    return redirect(url_for('UsersView:index'))
-
-  @route('/<int:id>/posts/', endpoint='user_post')
-  def user_post(self, id):
-    user = User.get_by_id(id)
-
-    if user is None:
-      flash(gettext('The user was not found'), 'error')
-      return redirect(url_for('UsersView:index'))
-
-    page = request.values.get('page', 1, type=int)
-    limit = 10
-
-    posts, total = user.get_user_posts(page=page, limit=limit)
-
-    return render_template("admin/users/user_posts.html",
-                           user=user,
-                           posts=posts,
-                           page=page,
-                           limit=limit,
-                           total=total)
+        return render_template("admin/users/user_posts.html",
+                               user=user,
+                               posts=posts,
+                               page=page,
+                               limit=limit,
+                               total=total)
