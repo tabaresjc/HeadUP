32d142fcc32dc770603833525089d8f4d8b80308
Juan Tabares <juan.ctt@live.com>
2017-01-06 18:23:15 +0900

    Add more control for the cache
    The cache can be enabled from the config file.

--- app/utils/cache.py
+++ app/utils/cache.py
@@ -2,6 +2,7 @@
 
 from flask_caching import Cache
 from flask_caching import make_template_fragment_key
+import config
 
 
 class CacheBase(Cache):
@@ -9,3 +10,21 @@ class CacheBase(Cache):
     def get_fragment(self, name, vary_on=None):
         key = make_template_fragment_key(name, vary_on=vary_on or [])
         return self.get(key)
+
+    @property
+    def cache_mode(self):
+        return getattr(config, 'CACHE_ENABLED', False)
+
+    def get(self, *args, **kwargs):
+        "Proxy function for internal cache object."
+        # only when cache is enabled via config
+        if not self.cache_mode:
+            return None
+        return Cache.get(self, *args, **kwargs)
+
+    def set(self, *args, **kwargs):
+        "Proxy function for internal cache object."
+        # only when cache is enabled via config
+        if not self.cache_mode:
+            return None
+        return Cache.set(self, *args, **kwargs)
