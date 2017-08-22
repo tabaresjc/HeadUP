931d380ad665318b41c8dd506209641d8b7880a2
Juan Tabares <juan.ctt@live.com>
2017-08-22 13:46:47 +0900

    Update GA
    update the code that generated the google analytics tracking code

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -34,6 +34,22 @@ def nl2br(eval_ctx, value):
     return result
 
 
+@app.template_filter()
+@evalcontextfilter
+def ga_autolinks(eval_ctx, items):
+    domain_name = request.headers.get('HOST', '')
+    if not items:
+        return None
+
+    result = []
+
+    for item in items:
+        if item != domain_name:
+            result.append(u'"%s"' % item)
+
+    return u','.join(result)
+
+
 @app.context_processor
 def utility_processor():
     # Site name
