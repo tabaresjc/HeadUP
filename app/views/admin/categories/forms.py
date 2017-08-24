9b586029a7d270d0781ce8010421d8094e36da8c
Juan Tabares <juan.ctt@live.com>
2017-08-24 16:43:50 +0900

    Update categories view

--- app/views/admin/categories/forms.py
+++ app/views/admin/categories/forms.py
@@ -4,41 +4,47 @@ from flask_wtf import Form
 from wtforms import TextField, TextAreaField, SelectField, validators
 from flask_babel import lazy_gettext, gettext
 from app.models import Category
+import re
 
 
 class CategoryForm(Form):
-    name = TextField(lazy_gettext('Name'), [validators.Required()])
-    slug = TextField(lazy_gettext('Slug'), [validators.Required()])
-    description = TextAreaField(lazy_gettext('Description'), [
-        validators.Length(min=0, max=255)
-    ])
+    name = TextField(lazy_gettext('Name'),
+                     [validators.Length(min=4, max=25),
+                      validators.InputRequired()])
 
+    slug = TextField(lazy_gettext('Slug'),
+                     [validators.Length(min=4, max=25),
+                      validators.InputRequired(),
+                      validators.Regexp('^[-\w]+$')])
 
-class NewCategoryForm(CategoryForm):
+    description = TextAreaField(lazy_gettext('Description'),
+                                [validators.Length(min=4, max=1024),
+                                 validators.InputRequired()])
 
-    def __init__(self, category, *args, **kwargs):
-        Form.__init__(self, *args, **kwargs)
-        self.name.data = category.name
-        self.slug.data = category.slug
-        self.description.data = category.description
+    def __init__(self, category=None, *args, **kwargs):
+        super(CategoryForm, self).__init__(*args, **kwargs)
+        self.set_category(category)
+
+    def set_category(self, category=None):
+        if category:
+            self.name.data = category.name
+            self.slug.data = category.slug
+            self.description.data = category.description
 
 
 class TranferCatForm(Form):
-    from_id = SelectField(lazy_gettext('From'), coerce=int,
-                          description=lazy_gettext('From'))
-    to_id = SelectField(lazy_gettext('To'), coerce=int,
-                        description=lazy_gettext('To'))
+    from_id = SelectField(lazy_gettext('From'), coerce=int)
+
+    to_id = SelectField(lazy_gettext('To'), coerce=int)
 
     def __init__(self, *args, **kwargs):
-        Form.__init__(self, *args, **kwargs)
+        super(Form, self).__init__(*args, **kwargs)
         cat_list = Category.get_list()
         self.from_id.choices = cat_list
         self.to_id.choices = cat_list
 
     def validate(self):
-        valid = True
-        if not Form.validate(self):
-            valid = False
+        valid = Form.validate(self)
 
         if self.from_id.data == self.to_id.data:
             self.to_id.errors.append(
@@ -46,9 +52,3 @@ class TranferCatForm(Form):
             valid = False
 
         return valid
-
-    def get_errors(self):
-        message = u''
-        for field in self.errors:
-            message += '<br/>'.join(self.errors[field])
-        return message
