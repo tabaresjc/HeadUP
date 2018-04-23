87b0740aabf4c912cdbe0f7121c34f39214c7eb4
Juan Tabares <juan.ctt@live.com>
2018-04-24 00:34:05 +0900

    improve js

--- static/assets/headsup/scripts/base/plugins/miscellaneous.js
+++ static/assets/headsup/scripts/base/plugins/miscellaneous.js
@@ -1,7 +1,8 @@
 define(['jquery', 'underscore'], function($, _) {
 	'use strict';
 
-	var mediumScreenSize = 991;
+	var mediumScreenSize = 991,
+		elementId;
 
 	function screenWidth() {
 		return (window.innerWidth > 0) ? window.innerWidth : screen.width;
@@ -18,5 +19,24 @@ define(['jquery', 'underscore'], function($, _) {
 		}, 200);
 	}
 
+	function stopScrolling() {
+		if (location.hash) {
+			elementId = location.hash;
+			window.scrollTo(0, 0);
+		}
+	}
+
+	function scrollToElement() {
+		if (elementId && $(elementId).length) {
+			var y = $(elementId).offset().top - 80;
+			$(document).scrollTop(y);
+		}
+	}
+
+	setTimeout(stopScrolling, 0);
 	$(window).on('resize', onResize);
+
+	$(function() {
+		setTimeout(scrollToElement, 0);
+	});
 });
