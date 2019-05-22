fffd89d5edd6da9d75a37fe11e0e678d6a53385a
Juan Tabares <juan.ctt@live.com>
2019-05-21 21:22:54 -0700

    update spinner

--- static/assets/helpers/spinner.js
+++ static/assets/helpers/spinner.js
@@ -6,23 +6,37 @@ export class SpinnerHelper {
 	}
 
 	_init() {
-		this._spinner = document.getElementById('spinner');
+		this._container = document.getElementById('spinner');
 
-		if (!this._spinner) {
-			this._spinner = document.createElement('div');
-			this._spinner.setAttribute('id', 'spinner');
-
-			document.body.appendChild(this._spinner);
+		if (!this._container) {
+			this._container = document.createElement('div');
+			this._container.setAttribute('id', 'overlay-container');
+			this._container.innerHTML = `
+			<div class="sk-circle">
+				<div class="sk-circle1 sk-child"></div>
+				<div class="sk-circle2 sk-child"></div>
+				<div class="sk-circle3 sk-child"></div>
+				<div class="sk-circle4 sk-child"></div>
+				<div class="sk-circle5 sk-child"></div>
+				<div class="sk-circle6 sk-child"></div>
+				<div class="sk-circle7 sk-child"></div>
+				<div class="sk-circle8 sk-child"></div>
+				<div class="sk-circle9 sk-child"></div>
+				<div class="sk-circle10 sk-child"></div>
+				<div class="sk-circle11 sk-child"></div>
+				<div class="sk-circle12 sk-child"></div>
+			</div>`;
+			document.body.appendChild(this._container);
 		}
 	}
 
 	start() {
-		this._spinner.classList.add('loading');
+		this._container.classList.add('loading');
 	}
 
 	stop(timeOut) {
 		setTimeout(() => {
-			this._spinner.classList.remove('loading');
+			this._container.classList.remove('loading');
 		}, timeOut || 250);
 	}
 }
