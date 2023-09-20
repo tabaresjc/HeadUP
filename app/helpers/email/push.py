786ce1b92cbba516713bd373d89de173155465f7
Juan Tabares <juan.tabares@staffbase.com>
2023-09-20 10:39:54 +0900

    update message

--- app/helpers/email/push.py
+++ app/helpers/email/push.py
@@ -1,6 +1,6 @@
 # -*- coding: utf8 -*-
 
-import logging
+from celery.utils.log import get_task_logger
 from flask_mail import Message
 from app.helpers.tasks import task_handler
 import app
@@ -8,7 +8,6 @@ import app
 
 @task_handler
 def push_email(subject, recipients, body, is_html=True):
-    from celery.utils.log import get_task_logger
     logger = get_task_logger(__name__)
     try:
         if not body or not subject or not recipients:
@@ -23,8 +22,9 @@ def push_email(subject, recipients, body, is_html=True):
             msg.html = body
         else:
             msg.body = body
-        logger.info('Sending email')
+
+        logger.info('[push_email] Sending email')
         app.mail.send(msg)
-        logger.info('Email sent')
+        logger.info('[push_email] Email sent')
     except Exception:
-        logger.exception('Failed to push_email')
+        logger.exception('[push_email] Failed to send email')
